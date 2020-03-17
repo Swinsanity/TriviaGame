e8475be3237277a94d4b5777fb256902d287aafe
Swinsanity <jesseswinsanity@gmail.com>
2020-03-16 22:02:36 -0400

    Created loops for questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,39 @@ var questions = [
     {
         prompt: "What was Ash Ketchum's first Pokemon?",
         choices: ["Bulbasaur", "Squirtle", "Charmander", "Pikachu"],
-        answer: "d"
+
+    },
+
+    {
+        prompt: "What region is Pacifidlog Town located in?",
+        choices: ["Kanto", "Hoenn", "Galar", "Unova"],
     }
 ]
 
-$(".question1").text(questions[0].prompt)
-var trivia = $(".trivia")
+for (var i = 0; i < questions.length; i++) {
 
-for(var i = 0; i < questions[0].choices.length; i++) {
-    
+    var prompt = $("<div/>").text(questions[i].prompt)
+
+    $("#questions").append(prompt)
+
+    var trivia = $("#questions");
+    for (var j = 0; j < questions[i].choices.length; j++) {
+        var input = $("<input/>");
+        var label = $("<label/>");
+        var br = $("<br/>");
+        input.attr({
+            type: "radio",
+            name: "pokemon",
+            value: questions[i].choices[j]
+
+        });
+        label.attr("for", questions[i].choices[j]);
+        label.text(questions[i].choices[j]);
+        trivia.append(input);
+        trivia.append(label);
+        trivia.append(br)
+
+    }
 }
+
+
